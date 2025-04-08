fcd26d69fc133ac59cf785c4c4796a2b4295a731
subhashree dasmohapatra <ssdmp.mn@gmail.com>
2025-04-08 12:33:00 +0530

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-urlspattern = [
+urlpattern = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts , name="post-page"),
-    path("posts/<slugs:slug>", views.post_details, name="post-details-page") # /posts/my-first-post
+    path("posts/<slug:slug>", views.post_details, name="post-details-page") # /posts/my-first-post
 
 ]
